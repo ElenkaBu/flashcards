fa393293ba1f28b7d7869e5985412dd6d4179a57
Elena <bubeevalena@gmail.com>
2022-09-09 11:38:10 +0300

    func getTopic

--- Model.js
+++ Model.js
@@ -1,5 +1,5 @@
 const { EventEmitter } = require('events');
-
+fs = require('fs').promises
 class Model extends EventEmitter {
   // сначала приложение находится на стартовой странице (выбор темы)
   // подумай, какие ещё страницы будут в твоём приложении?
@@ -8,7 +8,13 @@ class Model extends EventEmitter {
   getPage() {
     return this.#page;
   }
-
+  // Функция вывода списка тем:
+  async function getTopic() {
+    let countTopic = await fs.readdir('./topics')
+    countTopic = countTopic.map((el) => el.slice(0, -19))
+    console.log(countTopic);
+    return countTopic;
+  }
   chooseTopic(topic) {
     // тема выбрана, сделай необходимые изменения в модели (в т.ч. измени this.page)
     // ...
