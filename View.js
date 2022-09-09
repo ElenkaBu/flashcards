cd9df08e8f098c903b21fca9a731b212f1f77257
Kirill <kirpitch@inbox.ru>
2022-09-09 15:07:29 +0300

    Added question

--- View.js
+++ View.js
@@ -27,14 +27,14 @@ class View extends EventEmitter {
 
   renderStartPage() {
     // здесь попросим у модели список тем и предоставим пользователю выбор
-    let topics = this.#model.getTopic()
-    console.clear()
-    console.log('Пришло время выбрать тему:')
-    console.log()
+    let topics = this.#model.getTopic();
+    console.clear();
+    console.log('Пришло время выбрать тему:');
+    console.log();
     topics.forEach((element, i) => {
-        console.log(`${i + 1}. ${element}`)
+        console.log(`${i + 1}. ${element}`);
       });
-      console.log()
+      console.log();
       const topic = readlineSync.question('> ');
       this.emit('topicChosen', topic);
     }
@@ -46,26 +46,25 @@ class View extends EventEmitter {
     let count = 0;
     for (let i = 0; i < questions.length; i++) {
       let question = questions[i]
-      console.log(Object.keys(question).toString().toLowerCase());
+      console.log(Object.keys(question).toString());
       const answer = readlineSync.question('> ');
       if(answer) {
         if (answer.toLowerCase() === Object.values(question).toString().toLowerCase()) {
           count++
           console.clear();
-          console.log('Ты ответил правильно!');
+          console.log('Молодец, все верно!');
           console.log();
           continue
         } else {
           console.clear();
-
           console.log('Неправильный ответ!');
           console.log();
-       }
+        }
       }
     }
-    if (count === 0 || count === 1) console.log(`Твой результат ${count} из 5: в следующий раз попробуй подумать получше 🤷`);
-    if (count === 2 || count === 3) console.log(`Твой результат ${count} из 5: `);
-    if (count === 4 || count === 5) console.log(`Твой результат ${count} из 5: `);
+    if (count === 0 || count === 1) console.log(`Твой результат ${count} из 5: в следующий раз попробуй подумать получше, но главное не расстраивайся 😸`);
+    if (count === 2 || count === 3) console.log(`Твой результат ${count} из 5: В целом неплохо для начала 👻`);
+    if (count === 4 || count === 5) console.log(`Твой результат ${count} из 5: Мегамозг🤖`);
   }
 
 }
