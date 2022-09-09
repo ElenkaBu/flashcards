7915df9908b268a539e8a1af3263f838e7ddfeda
juantonova <j.konina@mail.ru>
2022-09-09 13:46:53 +0300

    Added second page

--- View.js
+++ View.js
@@ -1,6 +1,7 @@
 const { EventEmitter } = require('events');
 const readlineSync = require('readline-sync');
 
+
 class View extends EventEmitter {
   #model;
 
@@ -16,16 +17,53 @@ class View extends EventEmitter {
     // отображаем ту страницу, на которой мы сейчас находимся
     switch (this.#model.getPage()) {
       case 'start':
-        return this.renderStartPage();
-    }
+        this.renderStartPage();
+        break;
+      case 'questions':
+        this.renderQuestionPage();
+        break;
+    }   
   }
 
   renderStartPage() {
     // здесь попросим у модели список тем и предоставим пользователю выбор
-    // ...
-    // теперь уведомим контроллер о том что пользователь выбрал тему
-    this.emit('topicChosen', topic);
+    let topics = this.#model.getTopic()
+    console.clear()
+    console.log('Пришло время выбрать тему:')
+    console.log()
+    topics.forEach((element, i) => {
+        console.log(`${i + 1}. ${element}`)
+      });
+      console.log()
+      const topic = readlineSync.question('> ');
+      this.emit('topicChosen', topic);
+    }
+
+
+  renderQuestionPage() {
+    console.clear() 
+    let questions = this.#model.getQuestions();
+    for (let i = 0; i < questions.length; i++) {
+      let question = questions[i]
+      console.log(Object.keys(question).toString());
+      const answer = readlineSync.question('> ');
+      if(answer) {
+        if (answer === Object.values(question).toString()) {
+          console.clear()
+          console.log('Ты ответил правильно!')
+          console.log()
+          continue;
+        } else {
+          {
+            console.clear()
+            console.log('Неправильный ответ!')
+            break;
+          }
+       }
+      }
+    }
   }
+
 }
 
 module.exports = View;
--- testView.js
+++ testView.js
@@ -1,4 +1,5 @@
 const { EventEmitter } = require('events');
+const { readdirSync, readlinkSync } = require('fs');
 const readline = require('readline');
 const Model = require('./Model');
 
@@ -34,13 +35,18 @@ class View extends EventEmitter {
   }
 
   
-  // render() {
-  //   // отображаем ту страницу, на которой мы сейчас находимся
-  //   switch (this.#model.getPage()) {
-  //     case 'start':
-  //       return this.renderStartPage();
-  //   }
-  // }
+  render() {
+    // отображаем ту страницу, на которой мы сейчас находимся
+    switch (this.#model.getPage()) {
+      case 'start':
+        this.renderStartPage();
+        break;
+    }
+    {
+       case 'questions':
+       this.renderQuestionPage();
+      }   
+  }
 
   renderStartPage() {
     // здесь попросим у модели список тем и предоставим пользователю выбор
@@ -56,15 +62,43 @@ class View extends EventEmitter {
       console.log()
     })
     // теперь уведомим контроллер о том что пользователь выбрал тему
-    const topic = readlineSync.question('> ');
+    // const topic = readlineSync.question('> ');
     this.emit('topicChosen', topic);
+  }
+
+  renderQuestionPage() {
+    console.clear() 
+    let questions = this.#model.getQuestions;
+    for (let i = 0; i < questions.length; i++) {
+      console.log(questions[i]);
+      // const question = readlineSync.question('> ')
+      if(question) {
+        console.log('Неправильный ответ!')
+      } else {
+        console.log('Ты ответил правильно!')
+      }
+    }
   }
 }
 
 module.exports = View;
 
 let model = new Model()
+model.questions = [
+  { 'Что является основным источником пищи выдры?': 'рыба' },
+  {
+    'Верно или нет? Выдры большую часть времени проводят на суше.': 'верно'
+  },
+  { 'Сколько существует видов выдр?': '13' },
+  { 'Верно или нет? Выдры родом из Австралии.': 'нет' },
+  {
+    'Верно или нет? Выдры изготавливают и используют инструменты.': 'верно'
+  },
+  {
+    'Какова средняя продолжительность жизни выдры в дикой природе?': '10'
+  }
+]
 
 let view = new View(model)
 
-view.renderStartPage()
+view.renderQuestionPage()
